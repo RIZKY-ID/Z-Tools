11e729ea785231bd15585c8d15b569a2b543801c
MESTREX~ID <67997175+RIZKY-ID@users.noreply.github.com>
2020-07-08 14:01:42 +0700

    Add files via upload

---
+++ Z-Tools.py
@@ -0,0 +1,164 @@
+import os
+import random
+import sys
+import time
+from time import sleep
+os.system('clear')
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+print ('loading..')
+sleep(0.1)
+mengetik('\33[0;32m> \33[32;1m> \33[0;36m> \33[31;1m> \33[30;1m> \33[33;1m> \33[1;33m>] \33[31;1m100%')
+os.system('clear')
+sleep(0.1)
+print
+mengetik('\33[0;32mSELAMAT datang di tools kami')
+print
+mengetik('\33[33;1mkumpulan tool dari beberapa sumber...apabila ada tool yg mati atau tidak berfungsi harap hubungi author kami')
+print
+mengetik('\33[31;1mauthor tidak bertanggung jawab bila terjadi hal yg tidak diinginkan dosa tanggung sendiri')
+sleep(1)
+os.system('clear')
+
+print ("\33[31;1m===========================================")
+print ("\33[0;32m|Author : MESTREX~ID                      |")
+print ("\33[0;32m|       : RIZKY_X                      |")
+print ("\33[0;32m|Instagram  : Rk.sah_kpo        |")
+print ("\33[0;32m|Github.com  : RIZKY-ID/Z-Tools       |")
+print ("\33[31;1m===========================================")
+print
+mengetik('             \33[36;1mnow loading..........')
+print ("\33[0;32m     ==============================")
+print ("\33[1;33m         silahkan pilih toolnya")
+print ("                                ")
+print ("\33[1;33m                   gan            ")
+print ("\33[0;32m     ==============================")
+print
+sleep(1)
+print ("\33[36;1m==============================")
+print ("\33[0;32m1). Ddos attacker  ")
+print ("\33[36;1m==============================")
+sleep(1)
+print ("\33[0;32m2). 200 tool ")
+print ("\33[36;1m==============================")
+sleep(1)
+print ("\33[0;32m3). virtex for whatsapps")
+print ("\33[36;1m==============================")
+print ("\33[0;32m4). HACK Fb Target/bruteforce")
+print ("\33[36;1m==============================")
+print ("\33[0;32m5). HACK INSTRAGRAM ")
+print ("\33[36;1m==============================")
+print ("\33[0;32m6). cctv hack")
+print ("\33[36;1m==============================")
+print ("\33[0;32m7). INSTALL TOOLS ")
+print ("\33[36;1m==============================")
+sleep(1)
+pilih = str(input('\33[31;1m[?] \33[33;1mpilih : '))
+
+if pilih == "1":
+        os.system('pkg install git')
+        os.system('pkg install python2')
+        os.system('git clone https://github.com/BlackCyberAnonim/B-ATTACKING')
+        print
+        print ("\33[36;1m=============================================")
+        print ("\33[31;1m IKUTI TULISAN DIBAWAH UNTUK JALANKAN script")
+        print ("\33[36;1m=============================================")
+        print
+        print ("\33[36;1m[+]==================[+]")
+        print ("\33[33;1m[+]\33[31;1mcd B-ATTACKING")
+        print ("\33[33;1m[+]\33[31;1msh v999.sh")
+        print ("\33[36;1m[+]==================[+]")
+if pilih == "2":
+        os.system('apt update && apt upgrade')
+        os.system('apt install figlet')
+        os.system('apt install toilet')
+        os.system('gem install lolcat')
+        os.system('pkg install git')
+        os.system('pkg install python2')
+        os.system('git clone https://github.com/InYourG00D1/InstallPlerN2')
+        print
+        print ("\33[36;1m=============================================")
+        print ("\33[31;1m IKUTI TULISAN DIBAWAH UNTUK JALANKAN script")
+        print ("\33[36;1m=============================================")
+        print
+        print ("\33[36;1m[+]==================[+]")
+        print ("\33[33;1m[+]\33[31;1mcd InstallPlerN2")
+        print ("\33[33;1m[+]\33[31;1msh InstallPlerV2.sh")
+        print ("\33[36;1m[+]==================[+]")
+if pilih == "3":
+        os.system('pkg install git')
+        os.system('pkg install python2')
+        os.system('git clone https://github.com/muhammadfathul/VIRTEX')
+        print
+        print ("\33[36;1m=============================================")
+        print ("\33[31;1m IKUTI TULISAN DIBAWAH UNTUK JALANKAN script")
+        print ("\33[36;1m=============================================")
+        print
+        print ("\33[36;1m[+]==================[+]")
+        print ("\33[33;1m[+]\33[31;1mcd VIRTEX")
+        print ("\33[33;1m[+]\33[31;1mchmod +x virtex.sh")
+        print ("\33[33;1m[+]\33[31;1msh virtex.sh")
+        print ("\33[36;1m[+]==================[+]")
+if pilih == "4":
+        os.system('pkg install git')
+        os.system('pkg install python2')
+        os.system('git clone https://github.com/FR13ND8/BRUTEFORCEnew')
+        print
+        print ("\33[36;1m=============================================")
+        print ("\33[31;1m IKUTI TULISAN DIBAWAH UNTUK JALANKAN script")
+        print ("\33[36;1m=============================================")
+        print
+        print ("\33[36;1m[+]==================[+]")
+        print ("\33[33;1m[+]\33[31;1mcd BRUTEFORCEnew")
+        print ("\33[33;1m[+]\33[31;1msh new.sh")
+        print ("\33[36;1m[+]==================[+]")
+if pilih == "5":
+        os.system('pkg install python2')
+        os.system('pip2 install cowsay')
+        os.system('pkg install cowsay')
+        os.system('pkg install git')
+        os.system('pkg install figlet')
+        os.system('pkg install nano')
+        os.system('git clone https://github.com/darkcurut08/darkcurut08')
+        print
+        print ("\33[36;1m=============================================")
+        print ("\33[31;1m IKUTI TULISAN DIBAWAH UNTUK JALANKAN script")
+        print ("\33[36;1m=============================================")
+        print
+        print ("\33[36;1m[+]==================[+]")
+        print ("\33[33;1m[+]\33[31;1mcd darkcurut08")
+        print ("\33[33;1m[+]\33[31;1msh Moreno77.sh")
+        print ("\33[36;1m[+]==================[+]")
+if pilih == "6":
+        os.system('pkg install git')
+        os.system('pip2 install requests')
+        os.system('pkg install python2')
+        os.system('git clone https://github.com/GUNAWAN18ID/cctv.git')
+        print
+        print ("\33[36;1m=============================================")
+        print ("\33[31;1m IKUTI TULISAN DIBAWAH UNTUK JALANKAN script")
+        print ("\33[36;1m=============================================")
+        print
+        print ("\33[36;1m[+]==================[+]")
+        print ("\33[33;1m[+]\33[31;1mcd cctv")
+        print ("\33[33;1m[+]\33[31;1mpython2 scanner.py")
+        print ("\33[36;1m[+]==================[+]")
+if pilih == "7":
+        os.system('pkg install figlet')
+        os.system('pkg install toilet')
+        os.system('pkg install git')
+        os.system('gem install lolcat')
+        os.system('pkg install python2')
+        os.system('git clone https://github.com/MrXXXTRI3WSL/Install-Tools.sh')
+        print
+        print ("\33[36;1m=============================================")
+        print ("\33[31;1m IKUTI TULISAN DIBAWAH UNTUK JALANKAN script")
+        print ("\33[36;1m=============================================")
+        print
+        print ("\33[36;1m[+]==================[+]")
+        print ("\33[33;1m[+]\33[31;1mcd Install-Tools.sh")
+        print ("\33[33;1m[+]\33[31;1msh Install-Tools.sh")
+        print ("\33[36;1m[+]==================[+]")
